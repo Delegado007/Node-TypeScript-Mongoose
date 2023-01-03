87e0a4f9e90d350e01c7ad00ba85b68b0dc8ca7c
Delegado <maxharper2013@gmail.com>
2023-01-03 00:11:38 -0300

    [ADD] connection whit mongoDB

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import server from './src/server'
+import { MongoClient } from "mongodb";
 import { LogError, LogSuccess } from './src/utils/logger';
 
 // * Enviroment Variables
@@ -16,3 +17,25 @@ server.listen(port, () => {
 server.on('error', (error) => {
   LogError(`[SERVER ERROR]: ${error}`)
 })
+
+// connect to mongoDB
+// Connection URI
+const uri = "mongodb://root:root@localhost:27017/?authMechanism=DEFAULT";
+
+// Create a new MongoClient
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Establish and verify connection
+    const database = await client.db("test-api");
+    const userCollection = await database.collection("users")
+    console.log("Connected successfully to server");
+    console.log(userCollection)
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
